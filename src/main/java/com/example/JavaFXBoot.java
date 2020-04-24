2cee7fa3b0c196348815727758fa0ad552028575
Andrei Timar <atimar999@gmail.com>
2020-04-24 15:38:05 +0300

    Create javafx Boot class for jar build

--- src/main/java/com/example/JavaFXBoot.java
+++ src/main/java/com/example/JavaFXBoot.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,21 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class Main extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+public class JavaFXBoot extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/org.example/main.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JavaFXBoot.main(args);
+    }
+}
