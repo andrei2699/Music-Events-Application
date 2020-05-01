32382361e737934b0e08e19e8a3a76550bc4a3c9
Tania Topciov <taniatopciov@yahoo.com>
2020-05-01 19:23:15 +0300

    [MEA-41] - Creare pagina login
    - Creat Layout Pagina

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void onLoginClick(ActionEvent actionEvent) {
+    }
+
+    public void onGoToRegisterPageClick(ActionEvent actionEvent) {
+        
+    }
+}
