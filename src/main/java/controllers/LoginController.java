16b1d3847512c471abe46f15755dd80e29be20f4
Tania Topciov <taniatopciov@yahoo.com>
2020-05-01 20:08:54 +0300

    [MEA-57] - Creare controller pagina

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -2,7 +2,11 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import main.SceneSwitchController;
+import services.ServiceInjector;
+import services.UserService;
 
 public class LoginController {
 
@@ -12,15 +16,21 @@ public class LoginController {
     @FXML
     public TextField passwordTextField;
 
+    @FXML
+    public Label incorrectCredentialsError;
+
 
     public void onLoginClick(ActionEvent actionEvent) {
+        UserService userService = ServiceInjector.getInstance().getUserService();
+        if(userService.validateUserCredentials(emailTextField.getText(), passwordTextField.getText()))
+            onSkipPageButtonClick(actionEvent);
     }
 
     public void onGoToRegisterPageClick(ActionEvent actionEvent) {
-
+        SceneSwitchController.getInstance().switchScene(SceneSwitchController.SceneType.RegisterScene);
     }
 
     public void onSkipPageButtonClick(ActionEvent actionEvent) {
-
+        SceneSwitchController.getInstance().switchScene(SceneSwitchController.SceneType.MainScene);
     }
 }
