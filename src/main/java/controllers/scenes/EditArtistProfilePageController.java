0cea912d658dc012eaf3eaca9b7be927e52ca9ee
Andrei Timar <atimar999@gmail.com>
2020-05-13 15:52:24 +0300

    [MEA-92] - Editare Profil Artist checkbox formatie nefunctional
    -reparat bug editare profil

--- src/main/java/controllers/scenes/EditArtistProfilePageController.java
+++ src/main/java/controllers/scenes/EditArtistProfilePageController.java
@@ -48,6 +48,8 @@ public class EditArtistProfilePageController extends AbstractEditProfilePageCont
                 artistModel = new ArtistModel(userModel.getId(), false, "");
             }
             artistService.createArtist(artistModel);
+
+            updateUIOnInitialize();
         }
     }
 
