2cee9d8b9d4564c87f007e29af1c6f813a2d30fe
Andrei Timar <atimar999@gmail.com>
2020-05-17 16:53:25 +0300

    [MEA-102] - Testare serviciu gestiune fisiere
    -curat clase pentru a incepe testarea

--- src/test/java/services/implementation/FileSystemManagerTest.java
+++ src/test/java/services/implementation/FileSystemManagerTest.java
@@ -1,40 +1,74 @@
 package services.implementation;
 
+import models.UserModel;
 import org.junit.*;
-import services.ServiceProvider;
+import org.mockito.Mockito;
+import repository.implemenation.UserRepository;
+import services.IUserService;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
 
 public class FileSystemManagerTest {
+//
+//    private FileSystemManager fileSystemManager;
+//    private IUserService userService;
+//
+//    @Before
+//    public void setUp() {
+//        userService = new UserServiceImpl(new UserRepository());
+//    }
+//
+//    @After
+//    public void tearDown() {
+//        fileSystemManager = null;
+//    }
+//
+//    @Test
+//    public void createJSONFiles() {
+//        try {
+//            fileSystemManager.createJSONFiles();
+//
+//            Assert.assertTrue("Folder data exists", Files.exists(FileSystemManager.getPathToFile("data")));
+//
+//            Assert.assertTrue("Users JSON file exists", Files.exists(FileSystemManager.getUsersFilePath()));
+//            Assert.assertTrue("Bars JSON file exists", Files.exists(FileSystemManager.getBarsFilePath()));
+//            Assert.assertTrue("Artists JSON file exists", Files.exists(FileSystemManager.getArtistsFilePath()));
+//            Assert.assertTrue("Events JSON file exists", Files.exists(FileSystemManager.getEventsFilePath()));
+//            Assert.assertTrue("Reservations JSON file exists", Files.exists(FileSystemManager.getReservationsFilePath()));
+//
+//        } catch (IOException e) {
+//            Assert.fail("Cannot create folder structure");
+//        }
+//    }
+//
+//    @Test
+//    public void test_readUsers() {
+//        try {
+//            UserRepository repository = Mockito.mock(UserRepository.class);
+//            when(repository.getAll()).thenReturn(new ArrayList<>());
+//            when(repository.readById(2)).thenReturn(new UserModel());
+//
+//            IUserService us = new UserServiceImpl(repository);
+//
+//            us.updateUser();
+//
+//            fileSystemManager.createJSONFiles();
+//
+//            Assert.assertTrue("Folder data exists", Files.exists(FileSystemManager.getPathToFile("data")));
+//
+//            Assert.assertTrue("Users JSON file exists", Files.exists(FileSystemManager.getUsersFilePath()));
+//            Assert.assertTrue("Bars JSON file exists", Files.exists(FileSystemManager.getBarsFilePath()));
+//            Assert.assertTrue("Artists JSON file exists", Files.exists(FileSystemManager.getArtistsFilePath()));
+//            Assert.assertTrue("Events JSON file exists", Files.exists(FileSystemManager.getEventsFilePath()));
+//            Assert.assertTrue("Reservations JSON file exists", Files.exists(FileSystemManager.getReservationsFilePath()));
+//
+//        } catch (IOException e) {
+//            Assert.fail("Cannot create folder structure");
+//        }
+//    }
 
-    private FileSystemManager fileSystemManager;
-
-    @Before
-    public void setUp() {
-        fileSystemManager = ServiceProvider.getFileSystemManager();
-    }
-
-    @After
-    public void tearDown() {
-        fileSystemManager = null;
-    }
-
-    @Test
-    public void createJSONFiles() {
-        try {
-            fileSystemManager.createJSONFiles();
-
-            Assert.assertTrue("Folder data exists", Files.exists(FileSystemManager.getPathToFile("data")));
-
-            Assert.assertTrue("Users JSON file exists", Files.exists(FileSystemManager.getUsersFilePath()));
-            Assert.assertTrue("Bars JSON file exists", Files.exists(FileSystemManager.getBarsFilePath()));
-            Assert.assertTrue("Artists JSON file exists", Files.exists(FileSystemManager.getArtistsFilePath()));
-            Assert.assertTrue("Events JSON file exists", Files.exists(FileSystemManager.getEventsFilePath()));
-            Assert.assertTrue("Reservations JSON file exists", Files.exists(FileSystemManager.getReservationsFilePath()));
-
-        } catch (IOException e) {
-            Assert.fail("Cannot create folder structure");
-        }
-    }
 }
