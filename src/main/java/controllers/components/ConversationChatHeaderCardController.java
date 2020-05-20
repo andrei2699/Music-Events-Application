64a8bddbde8127dae5edc0dc9b276fd20251e433
Andrei Timar <atimar999@gmail.com>
2020-05-20 12:34:43 +0300

    [MEA-49] - Creearea chat-ului
    -adaugat card pentru header conversatie

---
+++ src/main/java/controllers/components/ConversationChatHeaderCardController.java
@@ -0,0 +1,18 @@
+package controllers.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ConversationChatHeaderCardController {
+    @FXML
+    public ImageView profilePictureImage;
+    @FXML
+    public Label nameLabel;
+
+    @FXML
+    public void onMouseClicked(MouseEvent mouseEvent) {
+
+    }
+}
