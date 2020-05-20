6b2857e33e260cf7b9d997f260118c8443d96cc8
Andrei Timar <atimar999@gmail.com>
2020-05-20 17:39:30 +0300

    [MEA-61] - Creare controller pagina chat
    -redenumit clasa DiscussionMessage

--- src/main/java/models/other/DiscussionMessageModel.java
+++ src/main/java/models/other/DiscussionMessageModel.java
@@ -1,11 +1,11 @@
 package models.other;
 
-public class DiscussionMessage {
+public class DiscussionMessageModel {
     private final String date;
     private final String text;
     private final int senderId;
 
-    public DiscussionMessage(String date, String text, int senderId) {
+    public DiscussionMessageModel(String date, String text, int senderId) {
         this.date = date;
         this.text = text;
         this.senderId = senderId;
