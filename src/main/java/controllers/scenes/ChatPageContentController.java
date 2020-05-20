6ce4c893ac94e6b9e810b0815e0ee08fbb679761
Andrei Timar <atimar999@gmail.com>
2020-05-20 12:31:04 +0300

    [MEA-49] - Creearea chat-ului
    -adaugat pagina chat
    -adaugat controller pagina
    -adaugat cale spre pagina chat

---
+++ src/main/java/controllers/scenes/ChatPageContentController.java
@@ -0,0 +1,12 @@
+package controllers.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ChatPageContentController {
+    @FXML
+    public TableView discussionHeaderTableView;
+    @FXML
+    public TableColumn discussionHeaderTableColumn;
+}
--- src/main/java/main/ApplicationResourceStrings.java
+++ src/main/java/main/ApplicationResourceStrings.java
@@ -13,6 +13,7 @@ public final class ApplicationResourceStrings {
     public static final String EDIT_BAR_SCENE_CONTENT_FXML_PATH = "/fxml/scenes/sceneContents/editBarProfilePage.fxml";
     public static final String EDIT_ARTIST_SCENE_CONTENT_FXML_PATH = "/fxml/scenes/sceneContents/editArtistProfilePage.fxml";
     public static final String CREATE_EVENT_SCENE_CONTENT_FXML_PATH = "/fxml/scenes/sceneContents/createEventForm.fxml";
+    public static final String CHAT_SCENE_CONTENT = "/fxml/scenes/sceneContents/chatPageContent.fxml";
 
     public static final String EVENT_DETAILS_CARD_FXML_PATH = "/fxml/components/eventDetailsCard.fxml";
     public static final String BAR_DETAILS_CARD_FXML_PATH = "/fxml/components/barDetailsCard.fxml";
