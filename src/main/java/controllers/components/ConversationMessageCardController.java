9c3ca645ebc9973aee0ffca8cc2afa89a976f105
Andrei Timar <atimar999@gmail.com>
2020-05-20 12:59:17 +0300

    [MEA-49] - Creearea chat-ului
    -adaugat card mesaj
    -adaugat stiluri card mesaj

---
+++ src/main/java/controllers/components/ConversationMessageCardController.java
@@ -0,0 +1,12 @@
+package controllers.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class ConversationMessageCardController {
+    @FXML
+    public HBox messageHBox;
+    @FXML
+    public Label messageLabel;
+}
