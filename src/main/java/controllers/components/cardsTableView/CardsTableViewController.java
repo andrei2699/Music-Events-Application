21ae1874bf50d80e5bdf61a831c4cd5ee189f3cb
Andrei Timar <atimar999@gmail.com>
2020-05-22 10:58:56 +0300

    [MEA-89] - Reutilizare Tabel pentru cautare
    -creat clasa pentru TableView
    -creat controller

---
+++ src/main/java/controllers/components/cardsTableView/CardsTableViewController.java
@@ -0,0 +1,22 @@
+package controllers.components.cardsTableView;
+
+import controllers.components.DetailsTableConfigData;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import models.cards.TableCardModel;
+import utils.CardTableFiller;
+
+public class CardsTableViewController {
+    public TableView<TableCardModel> cardsTableView;
+
+    public TableColumn<TableCardModel, TableCardModel> cardsTableColumn;
+
+    public void setItems(ObservableList<TableCardModel> cardModels) {
+        cardsTableView.setItems(cardModels);
+    }
+
+    public void setColumnData(DetailsTableConfigData detailsTableConfigData) {
+        CardTableFiller.setTableData(cardsTableView, cardsTableColumn, detailsTableConfigData);
+    }
+}
