9fd404084946bee885600448a8698ab494a4c0a2
Tania Topciov <taniatopciov@yahoo.com>
2020-05-22 19:11:11 +0300

    [MEA-111] - Redirectionare catre pagina de chaturi la apasarea butonului
    - Adaugat atributul seen in clasa Message

--- src/main/java/models/other/Message.java
+++ src/main/java/models/other/Message.java
@@ -4,11 +4,13 @@ public class Message {
     private String date;
     private String text;
     private int sender_id;
+    private boolean seen;
 
     public Message(String date, String text, int sender_id) {
         this.date = date;
         this.text = text;
         this.sender_id = sender_id;
+        this.seen=false;
     }
 
     public String getDate() {
@@ -21,5 +23,13 @@ public class Message {
 
     public int getSender_id() {
         return sender_id;
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
     }
 }
