dcd178bcc8bfd55f8159a75dcbe30af41b693187
Tania Topciov <taniatopciov@yahoo.com>
2020-05-23 20:44:56 +0300

    [MEA-62] - Creare controller pagina chat
    - Suprascris metoda toString

--- src/main/java/models/cards/DiscussionHeaderCardModel.java
+++ src/main/java/models/cards/DiscussionHeaderCardModel.java
@@ -1,6 +1,10 @@
 package models.cards;
 
+import main.LoggedUserData;
 import models.DiscussionModel;
+import services.ServiceProvider;
+
+import java.util.List;
 
 public class DiscussionHeaderCardModel implements TableCardModel {
     private final DiscussionModel discussionModel;
@@ -19,5 +23,20 @@ public class DiscussionHeaderCardModel implements TableCardModel {
 
     public DiscussionHeaderCardModel getDiscussionHeaderCardModel() {
         return this;
+    }
+
+    public String toString() {
+        List<Integer> ids = discussionModel.getIds();
+
+        if (ids == null)
+            return "";
+
+        Integer currentUserId = LoggedUserData.getInstance().getUserModel().getId();
+
+        if (currentUserId == ids.get(0)) {
+            return ServiceProvider.getUserService().getUser(ids.get(1)).getName();
+        } else {
+            return ServiceProvider.getUserService().getUser(ids.get(0)).getName();
+        }
     }
 }
