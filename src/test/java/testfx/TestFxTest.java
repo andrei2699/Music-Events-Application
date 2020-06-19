7539b5fe539ae2845c9941d86826f49eb27b45a2
Andrei Timar <atimar999@gmail.com>
2020-06-19 19:16:46 +0300

    [MEA-build-repair] - Repair Build
    -adaugat test

---
+++ src/test/java/testfx/TestFxTest.java
@@ -0,0 +1,28 @@
+package testfx;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestFxTest extends ApplicationTest {
+
+    private Label label;
+
+    @Before
+    public void setUp() {
+        label = new Label();
+        label.setText("ASD");
+    }
+
+    @Test
+    public void test() {
+        assertEquals("ASD", label.getText());
+    }
+}
