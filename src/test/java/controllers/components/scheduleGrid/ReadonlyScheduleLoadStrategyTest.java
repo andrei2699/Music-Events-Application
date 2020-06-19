b0d235472d115b35dc87970c9ac16080d37cc218
Andrei Timar <atimar999@gmail.com>
2020-06-19 16:28:23 +0300

    [MEA-147] - Testarea pachetului controllers.components.scheduleGrid
    -testat clasa ReadonlyScheduleLoadStrategy

--- src/test/java/controllers/components/scheduleGrid/ReadonlyScheduleLoadStrategyTest.java
+++ src/test/java/controllers/components/scheduleGrid/ReadonlyScheduleLoadStrategyTest.java
@@ -1,7 +1,6 @@
 package controllers.components.scheduleGrid;
 
-import javafx.collections.FXCollections;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import models.other.Interval;
 import org.junit.Before;
@@ -14,21 +13,19 @@ import java.util.List;
 import static models.other.DaysOfWeek.*;
 import static org.junit.Assert.*;
 
-public class EditableScheduleLoadStrategyTest extends ApplicationTest {
-
-    private EditableScheduleLoadStrategy editableScheduleLoadStrategy;
+public class ReadonlyScheduleLoadStrategyTest extends ApplicationTest {
+    private ReadonlyScheduleLoadStrategy readonlyScheduleLoadStrategy;
     private HBox[][] gridHBoxes;
 
     @Before
     public void setUp() {
-        editableScheduleLoadStrategy = new EditableScheduleLoadStrategy();
+        readonlyScheduleLoadStrategy = new ReadonlyScheduleLoadStrategy();
         gridHBoxes = new HBox[NumberOfDays + 1][3];
 
         for (int column = 0; column < gridHBoxes.length; column++) {
             for (int row = 0; row < gridHBoxes[column].length; row++) {
                 gridHBoxes[column][row] = new HBox();
-                ComboBox<Integer> comboBox = new ComboBox<>();
-                comboBox.setItems(FXCollections.observableArrayList(editableScheduleLoadStrategy.hours));
+                Label comboBox = new Label();
                 gridHBoxes[column][row].getChildren().add(comboBox);
             }
         }
@@ -36,22 +33,22 @@ public class EditableScheduleLoadStrategyTest extends ApplicationTest {
 
     @Test
     public void testHoursArray() {
-        assertEquals(24, editableScheduleLoadStrategy.hours.length);
+        assertEquals(24, readonlyScheduleLoadStrategy.hours.length);
         for (int i = 0; i < 24; i++) {
-            assertEquals(i, editableScheduleLoadStrategy.hours[i].intValue());
+            assertEquals(i, readonlyScheduleLoadStrategy.hours[i].intValue());
         }
     }
 
     @Test
     public void testGetIntervalsFromGrid() {
-        assertNull(editableScheduleLoadStrategy.getIntervalsFromGrid(null));
-        assertEquals(7, editableScheduleLoadStrategy.getIntervalsFromGrid(gridHBoxes).size());
+        assertNull(readonlyScheduleLoadStrategy.getIntervalsFromGrid(null));
+        assertEquals(7, readonlyScheduleLoadStrategy.getIntervalsFromGrid(gridHBoxes).size());
     }
 
     @Test
     public void testFillScheduleGridPane() {
         try {
-            editableScheduleLoadStrategy.fillScheduleGridPane(null, null);
+            readonlyScheduleLoadStrategy.fillScheduleGridPane(null, null);
         } catch (NullPointerException e) {
             fail();
         }
@@ -59,30 +56,28 @@ public class EditableScheduleLoadStrategyTest extends ApplicationTest {
         List<Interval> intervalList = new ArrayList<>();
         intervalList.add(new Interval(Luni, null, null));
         intervalList.add(new Interval(Marti, 14, 21));
-        intervalList.add(new Interval(Miercuri, 18, 20));
-        intervalList.add(new Interval(Joi, 19, 22));
-        intervalList.add(new Interval(Duminica, null, null));
-        intervalList.add(new Interval(Vineri, null, null));
+        intervalList.add(new Interval(Miercuri, 14, 20));
+        intervalList.add(new Interval(Joi, null, null));
+        intervalList.add(new Interval(Vineri, 15, 22));
         intervalList.add(new Interval(Sambata, null, null));
         intervalList.add(new Interval(Duminica, null, null));
 
-        editableScheduleLoadStrategy.fillScheduleGridPane(gridHBoxes, intervalList);
+        readonlyScheduleLoadStrategy.fillScheduleGridPane(gridHBoxes, intervalList);
 
         for (int column = 1; column < gridHBoxes.length; column++) {
             for (int row = 1; row < gridHBoxes[column].length; row++) {
-                assertEquals(ComboBox.class, gridHBoxes[column][row].getChildren().get(0).getClass());
-                ComboBox<?> comboBox = (ComboBox<?>) gridHBoxes[column][row].getChildren().get(0);
-                assertEquals(24, comboBox.getItems().size());
+                assertEquals(Label.class, gridHBoxes[column][row].getChildren().get(0).getClass());
+                Label label = (Label) gridHBoxes[column][row].getChildren().get(0);
 
                 Interval interval = intervalList.get(column - 1);
                 if (interval != null) {
                     if (row == 1) {
                         if (interval.getStart_hour() == null) {
-                            assertNull(comboBox.getValue());
+                            assertTrue(label.getText().isEmpty());
                         }
                     } else if (row == 2) {
                         if (interval.getEnd_hour() == null) {
-                            assertNull(comboBox.getValue());
+                            assertTrue(label.getText().isEmpty());
                         }
                     }
                 }
